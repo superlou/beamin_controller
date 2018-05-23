60828a6e8475d4777f8dcee47034068e0d869bd7
superlou <lousimons@gmail.com>
2018-05-23 19:52:34 -0400

    Added selection of targets for commands

--- beamin_controller/beamin.py
+++ beamin_controller/beamin.py
@@ -9,19 +9,21 @@ from .packager import Packager
 from .ssdp import discover
 
 
-def get_targets(only_pingable=True):
+def get_targets(only_pingable=True, select=None):
     config = json.load(open('config.json'))
     targets = [Target(t['location']) for t in config['targets']]
 
     if only_pingable:
         targets = [target for target in targets if target.ping()]
 
+    if select:
+        targets = [target for target in targets
+                   if (target.hostname in select) or (target.ip in select)]
+
     return targets
 
 
-def list_targets():
-    targets = get_targets(only_pingable=False)
-
+def list_targets(targets):
     for i, target in enumerate(targets):
         status = target.check_status().name
         hostname = target.hostname or ''
@@ -29,14 +31,12 @@ def list_targets():
         print('{} {:<20} {:<15} {}'.format(i, hostname, ip, status))
 
 
-def start_targets():
-    targets = get_targets()
+def start_targets(targets):
     for target in targets:
         target.start()
 
 
-def stop_targets():
-    targets = get_targets()
+def stop_targets(targets):
     for target in targets:
         target.stop()
 
@@ -54,7 +54,7 @@ def package_group(packager, group):
     packager.add_matching(include, exclude)
 
 
-def push_node(group_names):
+def push_node(group_names, targets):
     config = json.load(open('config.json'))
     push_groups = config['push_groups']
 
@@ -75,7 +75,6 @@ def push_node(group_names):
     if len(group_names) > 0:
         print("Pushing data for group(s): {}".format(', '.join(group_names)))
 
-        targets = get_targets()
         for target in targets:
             target.push('node.zip')
 
@@ -90,13 +89,18 @@ class ExtendAction(argparse.Action):
 def parse_args(args=None):
     parser = argparse.ArgumentParser(description='Beamin Info-Beamer tools')
     parser.register('action', 'extend', ExtendAction)
-    parser.add_argument('-l', '--list', help='list targets', action='store_true')
+    parser.add_argument('-l', '--list', help='list all targets',
+                        action='store_true')
     parser.add_argument('-p', '--push', help='stop info-beamer',
                         nargs='*', action='extend')
+    parser.add_argument('-t', '--target', nargs='+', action='extend',
+                        help='apply commands to specified targets')
+    parser.add_argument('-d', '--discover', help='discover targets using SSDP',
+                        action='store_true')
+
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-s', '--start', help='start info-beamer', action='store_true')
     group.add_argument('-x', '--stop', help='stop info-beamer', action='store_true')
-    group.add_argument('-d', '--discover', help='discover targets using SSDP', action='store_true')
 
     args = parser.parse_args(args)
 
@@ -110,17 +114,22 @@ def main():
     args = parse_args()
 
     if args.list:
-        list_targets()
+        list_targets(get_targets(only_pingable=False))
         sys.exit()
 
+    if args.target:
+        targets = get_targets(select=args.target)
+    else:
+        targets = get_targets()
+
     if args.start:
-        start_targets()
+        start_targets(targets)
 
     if args.stop:
-        stop_targets()
+        stop_targets(targets)
 
     if args.push:
-        push_node(args.push)
+        push_node(args.push, targets)
 
     if args.discover:
         discover('beamin_target:control')
